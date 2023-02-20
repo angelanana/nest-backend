e2146bb9d22092d149b06914dd9537833f48a449
nana <nana.zheng@rccchina.com>
2023-02-20 15:50:03 +0800

    fix: 提交index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log('object');
-// export function getA (a) {
-//   return '1111'
-// }
+export function getA(a) {
+  return '1111';
+}
 // export function getB (a) {
 //   return '1111'
 // }
